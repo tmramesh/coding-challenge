5b051e7507fe83a36fb1a7888e37cf5a1ab8cb49
tmramesh <tmramesh@gmail.com>
2019-02-14 10:03:57 +0530

    Added Unit Test for API. MOQ used for mocking

--- LoanManagementService/LoanManagementService.Tests/LoanControllerTest.cs
+++ LoanManagementService/LoanManagementService.Tests/LoanControllerTest.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using LoanManagementService.Controllers;
 using LoanDAL;
 using LoanModel;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Moq;
 
 namespace LoanManagementService.Tests
 {
@@ -15,63 +18,87 @@ namespace LoanManagementService.Tests
     [TestClass]
     public class LoanControllerTest
     {
-        public LoanControllerTest()
+        /// <summary>
+        /// TEST method for Mock Get vs Actual Get
+        /// </summary>
+        [TestMethod]
+        public void GetLoans_For_ID_1_Moq_DAL()
         {
-            
-        }
+            // SetUp/ Arrange
+            var mock = new Mock<ILoanDataAccess>();
+            mock.Setup(p => p.GetLoanListDetails("1")).Returns(new List<Loan>() {
+                new Loan() {
+                           UserName = "Ramesh",
+                           LoanName = "Loan Name",
+                           LoanID = 1,
+                           Balance = 12,
+                           Interest = 21,
+                           EarlyPaymentFee =23,
+                           PayOutCarryOver = 333
+                } });
 
-        private TestContext testContextInstance;
+            // Act
+            LoanController loan = new LoanController(mock.Object);
+            var result = loan.GetLoans("1");
+
+            // Asert
+            Assert.IsNotNull(result);
+            // Assert.AreEqual(1, result.)
+        }
 
         /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
+        /// Validation for Invalid request  
+        /// </summary>
+        [TestMethod]
+        public void GetReturnsNotFound()
         {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
+            // Arrange
+            var mock = new Mock<ILoanDataAccess>();
+            mock.Setup(p => p.GetLoanListDetails("1")).Returns(new List<Loan>() {
+                new Loan() {
+                           UserName = "Ramesh",
+                           LoanName = "Loan Name",
+                           LoanID = 1,
+                           Balance = 12,
+                           Interest = 21,
+                           EarlyPaymentFee =23,
+                           PayOutCarryOver = 333
+                } });
+
+            // Act
+            var result = new LoanController(mock.Object);
+            IHttpActionResult actionResult = result.GetLoans("10");
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
         }
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
+        /// <summary>
+        /// Validation for getLoans BY ID 1
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void Compare_Result_Of_GetLoans_by_ID_1()
         {
-            ILoanDataAccess _loanDAL = new LoanDataAccess();
-            var loanCOntroller = new LoanController();
+            // SetUp/ Arrange
+            var mock = new Mock<ILoanDataAccess>();
+            mock.Setup(p => p.GetLoanListDetails("1")).Returns(new List<Loan>() {
+                new Loan() {
+                           UserName = "Ramesh",
+                           LoanName = "Loan Name",
+                           LoanID = 1,
+                           Balance = 12,
+                           Interest = 21,
+                           EarlyPaymentFee =23,
+                           PayOutCarryOver = 333
+                } });
 
-      
+            // Act
+            LoanController loan = new LoanController(mock.Object);
+            var result = loan.GetLoans("1") as OkNegotiatedContentResult<List<Loan>>; ;
 
-
-            //
-            // TODO: Add test logic here
-            //
+            // Asert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Content[0].LoanID);
         }
     }
 }
